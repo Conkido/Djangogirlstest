d087cc9f5e862f23471c9e416f6454e52f147e28
dilan <dilanurral@gmail.com>
2023-10-07 17:59:52 +0200

    show posts in HTML

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    allposts=Post.objects.all()
+    data_dictionary={}
+    for post in allposts:
+        data_dictionary[post.title]= post.text
+    print(data_dictionary)    
+
+    return render(request, 'blog/post_list.html',{"posts": data_dictionary} )
