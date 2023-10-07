399c35e493a99703e6a14d67a086f5368cdfde95
dilan <dilanurral@gmail.com>
2023-10-07 15:20:00 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,48 @@
+
+def welcome():
+    print("Welcome on board!")
+
+def printname(my_variable):
+    print("hello my " + my_variable)  
+
+dog_names = ["mimi", "conki", "blackie"]
+
+
+for new_variable in dog_names:
+    print(new_variable)
+
+
+for numbers in  [2,7,9]:
+    print(numbers * "dilan")
+
+for n in range(1,9):
+    print(n)
+
+
+
+
+""" def multiply(value1, value2):
+    print(value1 * value2)
+
+multiply(3, 10)
+
+
+def multi(value1, value2):
+    result = value1 * value2
+    
+
+    if len(result)>20:
+        print("Warning")
+
+    else:
+        print("Results")
+    print(result)       
+
+
+multi("string", 5) """
+
+
+
+
+
+
